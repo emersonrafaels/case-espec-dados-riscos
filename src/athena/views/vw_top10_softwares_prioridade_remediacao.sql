c75b683bee8b4fa8497f45ead965245206be091f
Emerson Rafael <emersona7x@hotmail.com>
2026-06-10 13:01:41 -0300

    [ADD] NEW VIEW

--- src/athena/views/vw_top10_softwares_prioridade_remediacao.sql
+++ src/athena/views/vw_top10_softwares_prioridade_remediacao.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE VIEW workspace_db_case_espec_dados_riscos.vw_top10_softwares_p
 
 SELECT
     software_instalado,
+    versao,
 
     COUNT(DISTINCT servidor) AS qtd_servidores_afetados,
 
@@ -27,38 +28,42 @@ SELECT
         END
     ) AS qtd_impactos_criticos,
 
-    (
-        COUNT(DISTINCT servidor) * 5
-        +
-        SUM(
-            CASE
-                WHEN lower(rating) IN ('alto','médio','medio')
-                THEN 1
-                ELSE 0
-            END
-        ) * 3
-        +
-        SUM(
-            CASE
-                WHEN lower(impacto) LIKE '%cr%'
-                THEN 1
-                ELSE 0
-            END
-        ) * 4
-        +
-        COALESCE(AVG(indice_obsolescencia),0) * 10
-        +
-        COALESCE(AVG(criticidade),0) * 10
+    ROUND(
+        (
+            COUNT(DISTINCT servidor) * 5
+            +
+            SUM(
+                CASE
+                    WHEN lower(rating) IN ('alto','médio','medio')
+                    THEN 1
+                    ELSE 0
+                END
+            ) * 3
+            +
+            SUM(
+                CASE
+                    WHEN lower(impacto) LIKE '%cr%'
+                    THEN 1
+                    ELSE 0
+                END
+            ) * 4
+            +
+            COALESCE(AVG(indice_obsolescencia),0) * 10
+            +
+            COALESCE(AVG(criticidade),0) * 10
+        ),
+        2
     ) AS score_prioridade_remediacao
 
 FROM workspace_db_case_espec_dados_riscos.gold_obsolescencia_por_servidor
 
 WHERE
     indice_obsolescencia > 0
-    OR lower(rating) <> 'up to date'
+    OR lower(coalesce(rating,'')) <> 'up to date'
 
 GROUP BY
-    software_instalado
+    software_instalado,
+    versao
 
 ORDER BY
     score_prioridade_remediacao DESC
